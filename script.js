0de120b9b5a25a43c6bef9692c31b676c4401c40
BlueNavy <aulyaazzahra.jpr@gmail.com>
2025-04-20 17:33:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById("maafkanBtn").addEventListener("click", function() {
-    alert("Terima kasih telah memaafkan saya. 🥹\nSemoga hubungan kita membaik kembali 💙");
+    alert("Makasii yaa udah mau maafin. 🥹\nKalo kedepannya ada salah lagi, kamu yang aku salahin ya:p 💙");
 });
