c600417c371075fb3da2281dab1dc654a85b6b28
BlueNavy <aulyaazzahra.jpr@gmail.com>
2025-04-20 19:20:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.getElementById("maafkanBtn").addEventListener("click", function() {
     alert("Makasii yaa udah mau maafin. 🥹\nKedepannya Kalo ada salah lagi, kamu yang aku salahin:p");
 });
+if (music.paused) {
+    music.play();
+    toggleBtn.textContent = 'Pause Musik';
+    toggleBtn.classList.add('playing');
+} else {
+    music.pause();
+    toggleBtn.textContent = 'Play Musik';
+    toggleBtn.classList.remove('playing');
+}
